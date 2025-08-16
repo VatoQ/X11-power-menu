e44391a26786605bd46d7d443c7f25103934fd72
Jonathan Enns <jonathanenns97@web.de>
2025-08-16 19:56:09 +0200

    put the binary into the config directory

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 gcc main.c -o powermenu $(pkg-config --cflags --libs xft cairo) -lX11
+
+path_to_i3="/home/$USER/.config/i3blocks/scripts"
+
+
+
+rm -rf $path_to_i3/powermenu
+
+cp powermenu $path_to_i3
